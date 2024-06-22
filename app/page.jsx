4e8c5687bc143118de8b7844eb94051b44ea167f
egamovj <egamovj90@gmail.com>
2024-06-22 14:59:11 +0500

    home page completed

--- app/page.jsx
+++ app/page.jsx
@@ -5,12 +5,13 @@ import { FiDownload } from "react-icons/fi";
 // components
 import Socials from "@/components/Socials";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Frontend Developer</span>
             <h1 className="h1">
               Hello I'm <br /> <span className="text-accent">Egamov J</span>
@@ -36,11 +37,12 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
