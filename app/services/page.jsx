69bd579dd399357a9600f88cc4fdc72719189e79
egamovj <egamovj90@gmail.com>
2024-06-22 15:37:51 +0500

    services page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,5 +1,58 @@
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, eiusmod tempor incididunt",
+    href: "",
+  },
+  {
+    num: "02",
+    title: "UI/UX Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, eiusmod tempor incididunt",
+    href: "",
+  },
+  {
+    num: "03",
+    title: "Logo Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, eiusmod tempor incididunt",
+    href: "",
+  },
+  {
+    num: "04",
+    title: "SEO",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, eiusmod tempor incididunt",
+    href: "",
+  },
+];
+
 const Services = () => {
-  return <div>Services page</div>;
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+        >
+          {services.map((service, index) => {
+            return <div key={index}></div>
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
